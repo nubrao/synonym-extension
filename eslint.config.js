70ba597ca2584320b758ba3d31ca097912503212
Bruno Camara <bruno.dacamara@softtek.com>
2024-06-19 13:48:32 -0300

    updates para adequação do manifest3

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+    "no-eval": "error",
+    "no-new-func": "error",
+    "no-implied-eval": "error"
+  }
+};
